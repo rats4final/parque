2f283ca76cc9cbe4fdd6ef5efe2cb848e3e7f040
Marc15975 <91290441+Marc15975@users.noreply.github.com>
2022-05-10 23:32:02 -0400

    Arreglo

--- app/Models/Servicios.php
+++ app/Models/Servicios.php
@@ -8,10 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Servicios extends Model
 {
     use HasFactory;
-<<<<<<< HEAD:app/Models/rolModelo.php
-    protected $table = 'roles';
-    protected $primaryKey = 'id_rol';
-    protected $fillable = ['nombre_rol','descripcion_rol'];
-=======
->>>>>>> 2859c2cf8cb2c6e4d8bfad84e0ed637711eecbb7:app/Models/Servicios.php
+
 }
