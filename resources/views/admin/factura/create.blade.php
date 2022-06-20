5cc72bbbb3c334ae4eca2253be42dd65b147e842
Marc15975 <91290441+Marc15975@users.noreply.github.com>
2022-06-20 05:14:31 -0400

    factura dinamicaaa

--- resources/views/admin/factura/create.blade.php
+++ resources/views/admin/factura/create.blade.php
@@ -185,7 +185,7 @@
 
 
 
-<!-- Modal -->
+<!-- Modal a -->
 
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
