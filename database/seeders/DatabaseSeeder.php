652b132b851ffb501a4d3911f75175b3527bf76f
rats4final <80012704+rats4final@users.noreply.github.com>
2022-06-29 01:30:18 -0400

    seeders xd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            UserSeeder::class,
-            RolSeeder::class
+            RolSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
